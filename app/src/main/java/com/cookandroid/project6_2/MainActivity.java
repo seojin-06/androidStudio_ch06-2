b2644a27c8979e1015ffb1636077fd9d07820df8
seojin-06 <seojin7376@khu.ac.kr>
2023-09-24 15:20:18 +0900

    commit

---
+++ app/src/main/java/com/cookandroid/project6_2/MainActivity.java
@@ -0,0 +1,40 @@
+package com.cookandroid.project6_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnStart, btnStop;
+        final ViewFlipper viewFlipper;
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStop = (Button) findViewById(R.id.btnStop);
+        viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewFlipper.startFlipping();
+                viewFlipper.setFlipInterval(1000);
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewFlipper.stopFlipping();
+            }
+        });
+
+    }
+}
